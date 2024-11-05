f920f425eac2a86dc5a3c2638c4c9d8f1975ee3c
David Sanchez <ricarvid_9319@hotmail.com>
2024-11-05 09:11:19 +0100

    Overwrite the default tabstop 4 to 2 for markdown files

---
+++ private_dot_config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+-- Author: David Sanchez Jacome
+
+local opt = vim.opt_local -- for conciseness
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.softtabstop = 2
+opt.shiftwidth = 2 -- 2 spaces for indent width
