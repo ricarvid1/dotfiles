c1739bf753239a14ab3d98d55814e961484c8c68
David Sanchez <ricarvid_9319@hotmail.com>
2025-04-24 17:02:21 +0200

    Improve lspconfig description

--- private_dot_config/nvim/lua/ricarvid/plugins/lsp/lspconfig.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/lsp/lspconfig.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "barreiroleo/ltex_extra.nvim",
+    "b0o/schemastore.nvim",
     { "antosha417/nvim-lsp-file-operations", config = true },
     { "j-hui/fidget.nvim", opts = {} },
     { "folke/neodev.nvim", opts = {} },
@@ -43,29 +44,29 @@ return {
         opts.desc = "Show LSP type definitions"
         keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-        opts.desc = "See available code actions"
-        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+        opts.desc = "[A]ctions"
+        keymap.set({ "n", "v" }, "<leader>c", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-        opts.desc = "Smart rename"
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+        opts.desc = "Smart re[N]ame"
+        keymap.set("n", "<leader>gn", vim.lsp.buf.rename, opts) -- smart rename
 
-        opts.desc = "Show buffer diagnostics"
+        opts.desc = "Buffer [D]iagnostics"
         keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-        opts.desc = "Show line diagnostics"
+        opts.desc = "Line [d]iagnostics"
         keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-        opts.desc = "Go to previous diagnostic"
+        opts.desc = "Previous diagnostic"
         keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
-        opts.desc = "Go to next diagnostic"
+        opts.desc = "Next diagnostic"
         keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-        opts.desc = "Show documentation for what is under cursor"
+        opts.desc = "Show do[K]umentation under cursor"
         keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-        opts.desc = "Restart LSP"
-        keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+        opts.desc = "[R]estart LSP"
+        keymap.set("n", "<leader>gr", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
       end,
     })
 
@@ -94,6 +95,20 @@ return {
       --     filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
       --   })
       -- end,
+      ["marksman"] = function()
+        -- configure markdown language server
+        lspconfig["marksman"].setup({
+          capabilities = capabilities,
+          filetypes = { "markdown" },
+        })
+      end,
+      ["pyright"] = function()
+        -- configure python language server
+        lspconfig["pyright"].setup({
+          capabilities = capabilities,
+          filetypes = { "python" },
+        })
+      end,
       ["texlab"] = function()
         -- configure the texlab language server
         lspconfig["texlab"].setup({
@@ -101,14 +116,74 @@ return {
           filetypes = { "tex" },
         })
       end,
+      -- ["ltex"] = function()
+      --   -- configure the texlab language server
+      --   lspconfig["ltex"].setup({
+      --     capabilities = capabilities,
+      --     on_attach = function() -- rest of your on_attach process.
+      --       require("ltex_extra").setup({
+      --         -- table <string> : languages for witch dictionaries will be loaded, e.g. { "es-AR", "en-US" }
+      --         -- https://valentjn.github.io/ltex/supported-languages.html#natural-languages
+      --         load_langs = { "en-US", "es" }, -- en-US as default
+      --       })
+      --     end,
+      --     filetypes = { "tex", "markdown", "plaintext" },
+      --   })
+      -- end,
       ["ltex"] = function()
         -- configure the texlab language server
-        lspconfig["ltex"].setup({
+        lspconfig["ltex_plus"].setup({
           capabilities = capabilities,
-          on_attach = function() -- rest of your on_attach process.
-            require("ltex_extra").setup({})
+          on_attach = function(client, bufnr)
+            -- rest of your on_attach process.
+            require("ltex_extra").setup({
+              -- table <string> : languages for witch dictionaries will be loaded, e.g. { "es-AR", "en-US" }
+              -- load_langs = { "en-US", "es" }, -- en-US as default
+            })
           end,
-          filetypes = { "tex", "markdown", "plaintext" },
+          -- filetypes = { "tex", "markdown", "plaintext", "gitcommit" },
+          settings = {
+            ltex = {
+              enabled = {
+                "bib",
+                "context",
+                "gitcommit",
+                "html",
+                "markdown",
+                "org",
+                "pandoc",
+                "plaintex",
+                "quarto",
+                "mail",
+                "mdx",
+                "rmd",
+                "rnoweb",
+                "rst",
+                "tex",
+                "latex",
+                "text",
+                "typst",
+                "xhtml",
+              },
+            },
+          },
+        })
+      end,
+      ["yamlls"] = function()
+        -- configure the yaml language server
+        lspconfig["yamlls"].setup({
+          settings = {
+            yaml = {
+              schemaStore = {
+                -- You must disable built-in schemaStore support if you want to use
+                -- this plugin and its advanced options like `ignore`.
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = "",
+              },
+              schemas = require("schemastore").yaml.schemas(),
+            },
+          },
         })
       end,
       ["lua_ls"] = function()
