de21ae6b22fc386521b0a9a72a88020a0dbe418a
David Sanchez <ricarvid_9319@hotmail.com>
2025-04-24 16:55:43 +0200

    Automatic linting setup

--- private_dot_config/nvim/lua/ricarvid/plugins/linting.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/linting.lua
@@ -10,7 +10,7 @@ return {
       -- javascriptreact = { "eslint_d" },
       -- typescriptreact = { "eslint_d" },
       -- svelte = { "eslint_d" },
-      python = { "pylint" },
+      -- python = { "pylint" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
@@ -22,8 +22,8 @@ return {
       end,
     })
 
-    vim.keymap.set("n", "<leader>l", function()
+    vim.keymap.set("n", "<leader>rl", function()
       lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
+    end, { desc = "Lint current file" })
   end,
 }
