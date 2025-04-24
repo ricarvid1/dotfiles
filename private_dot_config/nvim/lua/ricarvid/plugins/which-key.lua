e14ea20ec0033d7df95e96a958a3fb5e896e444d
David Sanchez <ricarvid_9319@hotmail.com>
2025-04-24 16:52:03 +0200

    Organize which keys in groups

--- private_dot_config/nvim/lua/ricarvid/plugins/which-key.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/which-key.lua
@@ -10,4 +10,29 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+  config = function()
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>f", group = "[F]ind" }, -- group
+      { "<leader>e", group = "[E]xplorer" },
+      { "<leader>x", group = "[X]Errors" },
+      { "<leader>s", group = "[S]plit" },
+      { "<leader>t", group = "[T]ab" },
+      { "<leader>w", group = "[W]orkspace" },
+      { "<leader>r", group = "[R]un" },
+      { "<leader>g", group = "[G]LSP" },
+      { "<leader>h", group = "[H]unk" },
+      { "<leader>m", group = "[M]arketplace" },
+      { "<leader>o", group = "[O]bsidian" },
+    })
+  end,
 }
