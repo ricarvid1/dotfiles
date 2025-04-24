6f56e2a08a5976927e7e6cc741f1a9990951c6b5
David Sanchez <ricarvid_9319@hotmail.com>
2025-04-24 17:00:58 +0200

    Update runner keymaps

--- private_dot_config/nvim/lua/ricarvid/plugins/code-runner.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/code-runner.lua
@@ -5,16 +5,25 @@ return {
       filetype = {
         python = "python3 -u",
       },
+      -- startinsert (see ':h inserting-ex')
+      startinsert = false,
     })
     vim.keymap.set("n", "<leader>rr", ":RunCode<CR>", { noremap = true, silent = false, desc = "[R]un Code" })
     vim.keymap.set("n", "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false, desc = "[R]un [F]ile" })
+    vim.keymap.set("n", "<leader>rt", ":RunFile tab<CR>", { noremap = true, silent = false, desc = "[R]un File [T]ab" })
+    vim.keymap.set("n", "<leader>rp", ":RunProject<CR>", { noremap = true, silent = false, desc = "[R]un [P]roject" })
+    vim.keymap.set("n", "<leader>rx", ":RunClose<CR>", { noremap = true, silent = false, desc = "[R]un [C]lose" })
     vim.keymap.set(
       "n",
-      "<leader>rft",
-      ":RunFile tab<CR>",
-      { noremap = true, silent = false, desc = "[R]un [F]ile [T]ab" }
+      "<leader>rjf",
+      ":CRFiletype<CR>",
+      { noremap = true, silent = false, desc = "Get [J]SON [F]ile" }
     )
-    vim.keymap.set("n", "<leader>rp", ":RunProject<CR>", { noremap = true, silent = false, desc = "[R]un [P]roject" })
-    vim.keymap.set("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false, desc = "[R]un [C]lose" })
+    vim.keymap.set(
+      "n",
+      "<leader>rjp",
+      ":CRProjects<CR>",
+      { noremap = true, silent = false, desc = "Get [J]SON [P]rojects" }
+    )
   end,
 }
