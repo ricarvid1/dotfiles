ea6f478d9970c3202622bfddd5aec5d981ca4d88
David Sanchez <ricarvid_9319@hotmail.com>
2025-04-24 16:56:03 +0200

    Update mapping for lazygit

--- private_dot_config/nvim/lua/ricarvid/plugins/lazygit.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/lazygit.lua
@@ -14,6 +14,6 @@ return {
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
-    { "<leader>g", "<cmd>LazyGit<cr>", desc = "Open git lazy" },
+    { "<leader>i", "<cmd>LazyGit<cr>", desc = "G[I]t" },
   },
 }
