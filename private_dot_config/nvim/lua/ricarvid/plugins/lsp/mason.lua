a66b5b31c5456906b78699e13999ab58c6c2a06a
David Sanchez <ricarvid_9319@hotmail.com>
2025-05-21 18:27:27 +0200

    Fix mason so we avoid compatibility issues

--- private_dot_config/nvim/lua/ricarvid/plugins/lsp/mason.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/lsp/mason.lua
@@ -1,7 +1,11 @@
 return {
   "williamboman/mason.nvim",
+  version = "^1.0.0",
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
+    {
+      "williamboman/mason-lspconfig.nvim",
+      version = "^1.0.0",
+    },
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
