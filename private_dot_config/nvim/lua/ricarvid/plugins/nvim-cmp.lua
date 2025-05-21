f6cdd9e3bf0a83325d2c40e6fe9b9cdbbaf697c7
David Sanchez <ricarvid_9319@hotmail.com>
2025-05-21 18:30:02 +0200

    Get current major version of snippets

--- private_dot_config/nvim/lua/ricarvid/plugins/nvim-cmp.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/nvim-cmp.lua
@@ -7,7 +7,7 @@ return {
     {
       "L3MON4D3/LuaSnip",
       -- follow latest release.
-      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      version = "*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
       -- install jsregexp (optional!).
       build = "make install_jsregexp",
     },
