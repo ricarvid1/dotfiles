ef0ed213d56238e63db56e8e1da71ef2db9080cc
David Sanchez <david.sanchez@ipronics.com>
2025-05-27 22:40:30 +0200

    update treesitter branch

--- private_dot_config/nvim/lua/ricarvid/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPre", "BufNewFile" },
+  branch = "master",
   build = ":TSUpdate",
   dependencies = {
     "windwp/nvim-ts-autotag",
