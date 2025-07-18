dea5f96ca71b3591bd9da90cec0b62498d8fdbda
David Sanchez <david.sanchez@ipronics.com>
2025-07-18 19:15:21 +0200

    Change keymap to accept copilot

--- private_dot_config/nvim/lua/ricarvid/plugins/blink-cmp.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/blink-cmp.lua
@@ -22,7 +22,7 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = "super-tab" },
+    keymap = { preset = "default" },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
