7547ef83ffec34566b4a5c59934aebbffd148352
David Sanchez <david.sanchez@ipronics.com>
2025-08-12 22:43:24 +0200

    change surround to mini

--- private_dot_config/nvim/lua/ricarvid/plugins/mini.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/mini.lua
@@ -1,5 +1,6 @@
 return {
   "echasnovski/mini.nvim",
+  version = false, -- Use the latest commit
   config = function()
     -- Better Around/Inside textobjects
     --
@@ -8,5 +9,7 @@ return {
     --  - yinq - [Y]ank [I]nside [N]ext [']quote
     --  - ci'  - [C]hange [I]nside [']quote
     require("mini.ai").setup({ n_lines = 500 })
+    -- Better Surround textobjects
+    require("mini.surround").setup()
   end,
 }
