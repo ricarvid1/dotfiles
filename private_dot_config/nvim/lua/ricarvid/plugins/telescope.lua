36e42386e0addf25e58bc98e252b70c05f00ec17
David Sanchez <david.sanchez@ipronics.com>
2025-11-19 17:36:51 +0100

    update telescope

--- private_dot_config/nvim/lua/ricarvid/plugins/telescope.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  tag = "v0.1.9",
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
