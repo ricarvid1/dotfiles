d6c2b33345a6b5f544e28df42ed4ebc990521e10
David Sanchez <david.r.sanjac@gmail.com>
2025-12-02 15:47:08 +0100

    update sessions config

--- private_dot_config/nvim/lua/ricarvid/plugins/sessions.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/sessions.lua
@@ -1,6 +1,9 @@
 return {
   "olimorris/persisted.nvim",
   event = "BufReadPre", -- this will only start session saving when an actual file was opened
+  opts = {
+    autosave = true,
+  },
   config = function()
     vim.keymap.set("n", "<leader>wr", "<cmd>SessionToggle<cr>", { desc = "Restore session" })
   end,
