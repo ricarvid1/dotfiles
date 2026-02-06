76dfdfe276497120a648b34ad30316954a287dd0
David Sanchez <david.sanchez@lucedaphotonics.com>
2026-02-06 17:50:39 +0100

    tmux navigator config

--- private_dot_config/nvim/lua/ricarvid/plugins/init.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/init.lua
@@ -1,5 +1,22 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
   { "nvim-tree/nvim-web-devicons", opts = {} },
 }
