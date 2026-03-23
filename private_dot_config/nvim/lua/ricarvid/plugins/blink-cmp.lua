83dd19de8f17c0423018527c1e4b1632cf9f21cd
David Sanchez <david.sanchez@lucedaphotonics.com>
2026-03-23 11:55:22 +0100

    add cc to sources

--- private_dot_config/nvim/lua/ricarvid/plugins/blink-cmp.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/blink-cmp.lua
@@ -40,6 +40,9 @@ return {
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { "lsp", "snippets", "buffer", "path" },
+      per_filetype = {
+        codecompanion = { "codecompanion" },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
