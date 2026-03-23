8f768510a50d5727b5bdfcca981e7a2c8654808a
David Sanchez <david.sanchez@lucedaphotonics.com>
2026-03-23 11:57:51 +0100

    remove telescope functionality

--- private_dot_config/nvim/lua/ricarvid/plugins/telescope.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/telescope.lua
@@ -43,12 +43,12 @@ return {
     -- keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
     keymap.set("n", "<leader>fl", builtin.builtin, { desc = "Find Select Telescope" })
     keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find current Word" })
-    keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find by Grep" })
+    -- keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find by Grep" })
     keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Find Diagnostics" })
-    keymap.set("n", "<leader>fr", builtin.resume, { desc = "Find Resume" })
+    -- keymap.set("n", "<leader>fr", builtin.resume, { desc = "Find Resume" })
     keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = 'Find Recent Files ("." for repeat)' })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Find existing buffers" })
+    -- keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Find existing buffers" })
 
     -- Slightly advanced example of overriding default behavior and theme
     keymap.set("n", "<leader>/", function()
