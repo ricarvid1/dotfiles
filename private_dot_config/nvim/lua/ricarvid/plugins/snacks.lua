71754457a11df3b6b70d0e0364e2e7d05e2f7b8e
David Sanchez <david.sanchez@lucedaphotonics.com>
2026-03-23 11:56:42 +0100

    enable debug

--- private_dot_config/nvim/lua/ricarvid/plugins/snacks.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
+    debug = { enabled = true },
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
