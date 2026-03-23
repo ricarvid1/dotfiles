65a671949811f14b28dd33486160e30857607682
David Sanchez <david.sanchez@lucedaphotonics.com>
2026-03-23 11:55:07 +0100

    fix keymap

--- private_dot_config/nvim/lua/ricarvid/plugins/aerial.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/aerial.lua
@@ -17,7 +17,7 @@ return {
       end,
     })
     -- You probably also want to set a keymap to toggle aerial
-    vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>", { desc = "Aerial" })
+    vim.keymap.set("n", "<leader>aa", "<cmd>AerialToggle!<CR>", { desc = "Aerial" })
   end,
   enabled = true,
 }
