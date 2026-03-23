84e4055601d5d04cd2faff8c25e6f8012340039a
David Sanchez <david.sanchez@lucedaphotonics.com>
2026-03-23 11:55:50 +0100

    show current line blame

--- private_dot_config/nvim/lua/ricarvid/plugins/gitsigns.lua
+++ private_dot_config/nvim/lua/ricarvid/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   opts = {
+    current_line_blame = true,
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
 
